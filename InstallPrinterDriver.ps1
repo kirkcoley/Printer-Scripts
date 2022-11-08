ea66f13cfa0c2b02bec93e8b644cfe15dae04bd3
Kirk <5759798+kirkcoley@users.noreply.github.com>
2022-11-07 18:21:50 -0600

    uninstall script, console output

--- InstallPrinterDriver.ps1
+++ InstallPrinterDriver.ps1
@@ -1,16 +1,22 @@
+Write-Host "Expanding archive..."
 # unzip driver package and chdir
-Expand-Archive ".\HPNV.zip"
-Set-Location ".\HPNV"
+New-Item -ItemType Directory -Path "C:\HPNV"
+Expand-Archive ".\HPNV.zip" -DestinationPath "C:\HPNV"
+Set-Location "C:\HPNV\HPNV"
 
-# get absolute path for pnputil
-$absPath = Get-Location
-$infPath = Join-Path -Path $absPath -ChildPath "\hpvyt11.inf"
-
+Write-Host "Staging driver to Windows Driver Store..."
 # stage and install driver
-pnputil.exe /add-driver $infPath
+pnputil.exe /add-driver "C:\HPNV\HPNV\hpvyt11.inf"
 
+Write-Host "Adding print driver, port, and printer..."
 Add-PrinterDriver -Name "HP Envy 4500 series"
 
-Add-PrinterPort -PrinterHostAddress "192.168.1.102"
+Add-PrinterPort -Name "hp_print_port" -PrinterHostAddress "192.168.1.102"
 
-Add-Printer -Name "HP Envy 4500 series" -DriverName "HP Envy 4500 series" -PortName "192.168.1.102"
+Add-Printer -Name "HP Envy 4500 series" -DriverName "HP Envy 4500 series" -PortName "hp_print_port"
+
+Write-Host "Cleaning up..."
+Set-Location -Path "C:\" 
+Remove-Item -Recurse -Path "C:\HPNV"
+
+Write-Host "Done."
---
+++ UninstallPrinterDriver.ps1
@@ -0,0 +1,19 @@
+﻿Write-Host "Removing printer, driver, and port..."
+Remove-Printer -Name "HP Envy 4500 series"
+
+Remove-PrinterDriver -Name "HP Envy 4500 series"
+
+Remove-PrinterPort -Name "hp_print_port"
+
+Write-Host "Deleting drivers from store..."
+pnputil.exe /delete-driver oem32.inf
+pnputil.exe /delete-driver oem34.inf
+pnputil.exe /delete-driver oem38.inf
+
+Write-Host "Deleting temporary filesystem items..."
+Remove-Item -Recurse -Path C:\HPNV\HPNV
+Remove-Item -Path "C:\Users\kirkc\OneDrive\Desktop\driverjunk\HPNV.zip"
+
+Set-Location -Path "C:\Users\kirkc\OneDrive\Desktop\driverjunk"
+
+Write-Host "Done."
