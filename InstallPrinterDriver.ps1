4e34995d5e5450eea4ded65189c47fbac96eee9c
Kirk <5759798+kirkcoley@users.noreply.github.com>
2022-11-07 19:54:46 -0600

    Update InstallPrinterDriver.ps1

--- InstallPrinterDriver.ps1
+++ InstallPrinterDriver.ps1
@@ -1,22 +1,28 @@
-Write-Host "Expanding archive..."
-# unzip driver package and chdir
+Write-Output "Creating workspace..."
 New-Item -ItemType Directory -Path "C:\HPNV"
-Expand-Archive ".\HPNV.zip" -DestinationPath "C:\HPNV"
-Set-Location "C:\HPNV\HPNV"
+Set-Location "C:\HPNV"
 
-Write-Host "Staging driver to Windows Driver Store..."
+Write-Output "Downloading driver package..."
+Invoke-WebRequest -Method Get -Uri "https://github.com/kirkcoley/printer-drivers/releases/download/driver/HPNV.zip" -OutFile ".\HPNV.zip"
+
+Write-Output "Expanding archive..."
+# unzip driver package and chdir
+Expand-Archive ".\HPNV.zip" -DestinationPath "C:\HPNV"
+
+
+Write-Output "Staging driver to Windows Driver Store..."
 # stage and install driver
 pnputil.exe /add-driver "C:\HPNV\HPNV\hpvyt11.inf"
 
-Write-Host "Adding print driver, port, and printer..."
+Write-Output "Adding print driver, port, and printer..."
 Add-PrinterDriver -Name "HP Envy 4500 series"
 
-Add-PrinterPort -Name "hp_print_port" -PrinterHostAddress "192.168.1.102"
+Add-PrinterPort -Name "hp_print_port" -PrinterOutputAddress "192.168.1.102"
 
 Add-Printer -Name "HP Envy 4500 series" -DriverName "HP Envy 4500 series" -PortName "hp_print_port"
 
-Write-Host "Cleaning up..."
+Write-Output "Cleaning up..."
 Set-Location -Path "C:\" 
 Remove-Item -Recurse -Path "C:\HPNV"
 
-Write-Host "Done."
+Write-Output "Done."
